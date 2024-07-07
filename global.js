1742ca6acf7c98dcbeff272d811c08ce766acde6
nickletipping <167684056+nickletipping@users.noreply.github.com>
2024-07-07 23:12:41 +0300

    Update global.js

--- global.js
+++ global.js
@@ -1,28 +1,16 @@
-const topAd=document.querySelector('a[href="https://www.a2hosting.com/?aid=adflyrich&amp;bid=a3194333"]');
+document.addEventListener("DOMContentLoaded", function() {
+    const topAd = document.querySelector('a[href="https://www.a2hosting.com/?aid=adflyrich&bid=a3194333"]');
 
+    const arrayOfAdTags = [
+      `<a href="https://www.a2hosting.com/?aid=adflyrich&bid=a3194333" target="_top"><img src="//affiliates.a2hosting.com/accounts/default1/9u7wabf7le4/a3194333.jpg" alt="" title="" style="max-width:90vw" width="728" height="90"></a>`,
+      `<a href="https://www.a2hosting.com/wordpress-hosting/managed/?aid=adflyrich&bid=0ffde563" target="_top"><img src="//affiliates.a2hosting.com/accounts/default1/9u7wabf7le4/0ffde563.jpg" alt="managed wordpress hosting" title="managed wordpress hosting" width="728" height="90" /></a><img style="border:0;max-width:90vw;" src="https://affiliates.a2hosting.com/scripts/9u7waif7le4?aid=adflyrich&bid=0ffde563" width="1" height="1" alt="" />`,
+      `<a href="https://www.a2hosting.com/joomla-hosting/?aid=adflyrich&bid=1debff52" target="_top"><img src="//affiliates.a2hosting.com/accounts/default1/9u7wabf7le4/1debff52.png" alt="joomla hosting" title="joomla hosting" width="728" height="90" /></a><img style="border:0;max-width:90vw;" src="https://affiliates.a2hosting.com/scripts/9u7waif7le4?aid=adflyrich&bid=1debff52" width="1" height="1" alt="" />`,
+      `<a href="https://www.a2hosting.com/opencart-hosting/?aid=adflyrich&bid=9262d080" target="_top"><img src="//affiliates.a2hosting.com/accounts/default1/9u7wabf7le4/9262d080.gif" alt="" title="" width="728" height="90" /></a><img style="border:0;max-width:90vw;" src="https://affiliates.a2hosting.com/scripts/9u7waif7le4?aid=adflyrich&bid=9262d080" width="1" height="1" alt="" />`
+    ];
 
-const arrayOfAdTags=[
+    const randomness = () => Math.floor(Math.random() * arrayOfAdTags.length);
 
-  `<a href="https://www.a2hosting.com/?aid=adflyrich&amp;bid=a3194333" target="_top"><img src="//affiliates.a2hosting.com/accounts/default1/9u7wabf7le4/a3194333.jpg" alt="" title="" style="max-width:90vw" width="728" height="90"></a>`,
-  
-`<a href="https://www.a2hosting.com/wordpress-hosting/managed/?aid=adflyrich&amp;bid=0ffde563" target="_top"><img src="//affiliates.a2hosting.com/accounts/default1/9u7wabf7le4/0ffde563.jpg" alt="managed wordpress hosting" title="managed wordpress hosting" width="728" height="90" /></a><img style="border:0;max-width:90vw;" src="https://affiliates.a2hosting.com/scripts/9u7waif7le4?aid=adflyrich&amp;bid=0ffde563" width="1" height="1" alt="" />`
-,
-  `<a href="https://www.a2hosting.com/joomla-hosting/?aid=adflyrich&amp;bid=1debff52" target="_top"><img src="//affiliates.a2hosting.com/accounts/default1/9u7wabf7le4/1debff52.png" alt="joomla hosting" title="joomla hosting" width="728" height="90" /></a><img style="border:0;max-width:90vw;" src="https://affiliates.a2hosting.com/scripts/9u7waif7le4?aid=adflyrich&amp;bid=1debff52" width="1" height="1" alt="" />1`
-,
-  `<a href="https://www.a2hosting.com/opencart-hosting/?aid=adflyrich&amp;bid=9262d080" target="_top"><img src="//affiliates.a2hosting.com/accounts/default1/9u7wabf7le4/9262d080.gif" alt="" title="" width="728" height="90" /></a><img style="border:0;max-width:90vw;" src="https://affiliates.a2hosting.com/scripts/9u7waif7le4?aid=adflyrich&amp;bid=9262d080" width="1" height="1" alt="" />`
- 
-
-
-  ];
-
-
-const randomness=()=>{return Math.floor(Math.random()*arrayOfAdTags.length)};
-
-
-if(topAd){
-
-topAd.parentNode.innerHTML=arrayOfAdTags[randomness];
-
-
-
-}
+    if (topAd) {
+      topAd.parentNode.innerHTML = arrayOfAdTags[randomness()];
+    }
+  });
